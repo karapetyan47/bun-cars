b0e280a14a293ade47df0fac2851ce4ed3f0a0e1
karapetyan47 <karapetyan4747@gmail.com>
2025-03-14 13:53:06 +0400

    Added s3 folder in gitignore

--- merge-schemas.sh
+++ merge-schemas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Merge all .prisma files into one
-cat prisma/schemas/*.prisma > prisma/schema.prisma
+cat src/prisma/schemas/*.prisma > src/prisma/schema.prisma
 
 echo "Schemas merged successfully!"
